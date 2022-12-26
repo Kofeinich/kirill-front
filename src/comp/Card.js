91e7254fb4374fa75dc28f611c82bd57b2d74929
Kofeinich <kofeinich1@gmail.com>
2022-12-26 17:14:50 +0300

    feat: routes working

---
+++ src/comp/Card.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Flex} from "chakra-ui";
+
+export const Card = () => {
+
+    const mock = [ { "id": 1, "name": "пончик", "price": 120 }, { "id": 2, "name": "вафли", "price": 140 }, { "id": 3, "name": "сок", "price": 100 }, { "id": 4, "name": "колбаса", "price": 300 }, { "id": 5, "name": "мыло", "price": 200 }, { "id": 6, "name": "вишня", "price": 30 } ]
+
+    return <Flex
+        w={'320px'}
+        borderRadius={'8px'}
+
+    >
+    </Flex>
+}
